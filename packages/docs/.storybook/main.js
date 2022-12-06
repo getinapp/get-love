ad0b75da2344ad9282c4d65f8f12ad9bbec064ca
maganezf <maganezfilho@gmail.com>
2022-12-06 16:28:38 -0300

    style(docs): storybook fonts

--- packages/docs/.storybook/main.js
+++ packages/docs/.storybook/main.js
@@ -21,45 +21,4 @@ module.exports = {
     if (configType === 'PRODUCTION') config.base = '/get-love/';
     return config;
   },
-  previewHead: head => `
-    ${head}
-    <style>
-      @font-face {
-        font-family: 'Maison Neue Extended';
-        font-display: swap;
-        src: local('Maison Neue Extended'),
-          url('../public/fonts/MaisonNeueExt-Medium.ttf') format('truetype');
-      }
-      @font-face {
-        font-family: 'Maison Neue Extended Bold';
-        font-display: swap;
-        src: local('Maison Neue Extended Bold'),
-          url('../public/fonts/MaisonNeueExt-Bold.ttf') format('truetype');
-      }
-      @font-face {
-        font-family: 'Noto Serif';
-        font-display: swap;
-        src: local('Noto Serif'),
-          url('../public/fonts/NotoSerif-Italic.ttf') format('truetype');
-      }
-
-      * {
-        margin: 0;
-        padding: 0;
-        box-sizing: border-box;
-      }
-
-      html,
-      :root {
-        font-size: 62.5%; /* 10px based */
-      }
-
-      body {
-        font-family: 'Maison Neue Extended';
-        font-size: 1.6rem;
-        -webkit-font-smoothing: antialiased;
-        -moz-osx-font-smoothing: grayscale;
-      }
-    </style>
-  `,
 };
